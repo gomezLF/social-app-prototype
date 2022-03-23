2027319e500072b71bc6a8a51f4b888b9004af50
Felipe Gómez <luis.andrade18@hotmail.com>
2022-03-23 14:06:41 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'social-app-prototype',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
