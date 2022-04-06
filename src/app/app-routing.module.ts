5b6605621f199af1bceec72791f90212cbbbf959
Felipe Gómez <luis.andrade18@hotmail.com>
2022-04-06 13:41:17 -0500

    Eliminadas las rutas de las páginas pertenecientes al tab bar (profile, home, publish)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,18 +16,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/singup/singup.module').then( m => m.SingupPageModule)
   },
   {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'publish',
-    loadChildren: () => import('./pages/publish/publish.module').then( m => m.PublishPageModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
-  },
-  {
     path: 'initial-tab',
     loadChildren: () => import('./pages/initial-tab/initial-tab.module').then( m => m.InitialTabPageModule)
   },
