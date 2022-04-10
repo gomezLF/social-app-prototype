a4425be867b57f19c2b4306c73c52a830a705038
Felipe Gómez <luis.andrade18@hotmail.com>
2022-04-10 18:49:12 -0500

    Llenados los campos con los datos del usuario actual

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,13 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  user: any;
+
+  constructor(private router: Router, private userService: UserService) {
+    this.user = userService;
+   }
 
   ngOnInit() {
   }
 
   changeProfileImageClicked() {
     console.log('hola');
+  }
+
+  moreOptionsClicked() {
+    this.router.navigate(['./login']);
   }
 
 }
