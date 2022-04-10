6080ca783ca2ff0bb8636268260e25313dc0d7d2
Felipe Gómez <luis.andrade18@hotmail.com>
2022-04-09 21:39:37 -0500

    Creadas las validaciones para el registro de usuario nuevo

--- src/app/pages/singup/singup.page.ts
+++ src/app/pages/singup/singup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-singup',
@@ -7,9 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingupPage implements OnInit {
 
-  constructor() { }
+  singupForm = this.formBuilder.group({
+    name: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required])
+  });
+
+  public errormessages = {
+    name: [
+      { type: 'required', message: 'El nombre es requerido'}
+    ],
+    lastname: [
+      { type: 'required', message: 'Los apellidos son requeridos'}
+    ],
+    email: [
+      { type: 'required', message: 'El correo electrónico es obligatorio'},
+      { type: 'pattern', message: 'Ingrese un correo electrónico válido'}
+    ],
+    password: [
+      { type: 'required', message: 'La contraseña es obligatoria'}
+    ],
+    confirmPassword: [
+      { type: 'required', message: 'Este campo es requerido' }
+    ]
+  };
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  get name() {
+    return this.singupForm.get('name');
+  }
+
+  get lastname() {
+    return this.singupForm.get('lastname');
+  }
+
+  get email() {
+    return this.singupForm.get('email');
+  }
+
+  get password() {
+    return this.singupForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.singupForm.get('confirmPassword');
+  }
 
   ngOnInit() {
   }
 
+  private matchValidator(controlValidationName: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      const controlValidation = control.root.get(controlValidationName);
+      if (!controlValidation) {
+        return null;
+      }
+      return controlValidation.value !== control.value ?
+        { matchValidator: { value: control.value } } : null;
+    };
+  }
 }
