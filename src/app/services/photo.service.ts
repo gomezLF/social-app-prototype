76f80a457c88fe7f0920ce9b4075bc8eaa38d455
Felipe Gómez <luis.andrade18@hotmail.com>
2022-04-12 17:22:45 -0500

    Creado el método que permite guardar y cargar las publicaciones

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { filter, switchMap } from 'rxjs/operators';
 import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
 import { BehaviorSubject, from, of } from 'rxjs';
-import { UserService } from './user.service';
 import { Camera, CameraResultType, CameraSource, ImageOptions, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
-import { Storage } from '@capacitor/storage';
+import { Storage } from '@ionic/storage-angular';
 
 const PUBLISH_KEY = 'publish';
 
@@ -15,7 +14,7 @@ export class PhotoService {
   // eslint-disable-next-line @typescript-eslint/no-inferrable-types
   photo: string = '';
 
-  pothos: Array<{
+  publish: Array<{
     id: string;
     pathPhoto: string;
     message: string;
@@ -26,19 +25,12 @@ export class PhotoService {
     time: string;
   }> = [];
 
-  currentPhoto: {
-    id: string;
-    pathPhoto: string;
-    message: string;
-    place: string;
-    idUser: string;
-    profileNameUser: string;
-    imageUser: string;
-    time: string;
-  };
+  private storageReady = new BehaviorSubject(false);
 
-  constructor() {
-    this.requestPermission();
+  constructor(private storage: Storage) {
+    //this.requestPermission();
+    this.init();
+    this.loadPublish();
   }
 
   async takephoto() {
@@ -67,7 +59,7 @@ export class PhotoService {
     const date = new Date();
 
     if(newId === 0) {
-      newId = this.pothos.length + 1;
+      newId = this.publish.length + 1;
     }
 
     const newPulication = {
@@ -81,9 +73,27 @@ export class PhotoService {
       time: '' + date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()
     };
 
-    this.pothos.push(newPulication);
+    this.publish.push(newPulication);
     this.photo = '';
-    console.log(this.pothos);
+    this.savePublish();
+    console.log(this.publish);
+  }
+
+  savePublish() {
+    this.storage.set(PUBLISH_KEY, this.publish);
+  }
+
+  loadPublish() {
+    return this.storageReady.pipe(
+      filter(ready => ready),
+      switchMap(_ => from(this.storage.get(PUBLISH_KEY)) || of([]))
+    );
+  }
+
+  private init() {
+    this.storage.defineDriver(CordovaSQLiteDriver);
+    this.storage.create();
+    this.storageReady.next(true);
   }
 
   private requestPermission() {
