55a0b33eede4fcec455b58f59ce03ab85cc62313
Felipe Gómez <luis.andrade18@hotmail.com>
2022-04-12 18:43:22 -0500

    Agregada la vista home que permite ver las publicaciones

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  publish: PhotoService;
+
+  constructor(private publishService: PhotoService) {
+    this.publish = this.publishService;
+   }
 
   ngOnInit() {
+
   }
 
 }
